0571727799d7d078db095a330218c1b364bfa0c2
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-21 21:04:05 +0530

    Shell - expense-mysql installations

--- mysql.sh
+++ mysql.sh
@@ -4,14 +4,15 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%s)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "Please enter DB Password"
+read -s mysql_root_password
 
 VALIDATE(){
-  if [ $1 -ne 0]
+  if [ $1 -ne 0 ]
   then
     echo -e "$2 ..... $R FAILURE $N"
     exit 1
@@ -20,7 +21,7 @@ VALIDATE(){
   fi
 }
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
   echo "Please run the script with the super user"
   exit 1
@@ -37,5 +38,15 @@ VALIDATE $? "Enabling MySql Server"
 systemctl start mysqld &>>LOGFILE
 VALIDATE $? "Starting MySql Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#VALIDATE $? "Setting up root password"
+
+#Below code will be useful for idempotent nature
+mysql -h db.devops91.in -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+  mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+  VALIDATE $? "MySQL Root password setup"
+else
+  echo -e  "MySql Root password is already setup .... $Y SKIPPING $N "
+fi
